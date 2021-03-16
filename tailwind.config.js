1749bf70e7610c0c5eaa94dea53879d8659b7273
derek_reilly <derek.reilly@inmeta.no>
2021-03-16 21:04:55 +0100

    Created storybloc components

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            'sans': 'Roboto, Arial, sans-serif',
+            'serif': 'Merriweather, Georgia, serif'
+        },
+        extend: {
+        },
+    },
+    variants: {},
+    plugins: [],
+}
